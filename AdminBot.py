57198d331792dbddde28689c1df3f76413fc0fce
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2021-04-15 12:05:41 +0530

    Update AdminBot.py

--- AdminBot.py
+++ AdminBot.py
@@ -1,8 +1,9 @@
 from telegram import *
 from telegram.ext import *
 import MainBot
+import random
 
-APIKey = "<-- Your Key -->"
+APIKey = "<-- User Key -->"
 MyBot = Bot(APIKey)
 
 UpdateMyBot = Updater(APIKey,use_context=True)
@@ -36,16 +37,25 @@ def AdminOwnership(update:Update,context:CallbackContext):
         )
     return ConversationHandler.END
 
+def userIDlist():
+    IDList = []
+    ID = open("userID.txt",'r')
+    userId = ID.readlines()
+    for i in userId:
+        IDList.append(str(i.replace("\n", "")))
+    return IDList
+
 def isAllow(id):
     AccessPassAllow = open("SpecialControlPass.txt",'r')
     idpass = AccessPassAllow.readlines()
     status = 0
+    adminList = []
     for i in idpass:
-        i = i.replace("\n", "")
-        if(str(i) == str(id)):
-            status = 1
-        else:
-            status = 0
+        adminList.append(str(i.replace("\n", "")))
+    if(str(id) in adminList):
+        status = 1
+    else:
+        status = 0
     if(status == 1):
         return True
     else:
@@ -54,6 +64,7 @@ def isAllow(id):
 
 def control(update:Update,context:CallbackContext):
     commandGiven = update.message.text
+    commandGiven.lower()
     if(commandGiven in ["bye","bubye","byeeeee","Bye","ttyl"]):
         MyBot.send_message(
             chat_id = update.effective_chat.id,
@@ -82,3 +93,31 @@ def adminGide(update:Update,context:CallbackContext):
                 chat_id = update.effective_chat.id,
                 text = "This access are only given to Admin.So, Sorry!"
             )
+sendnoticetousers = range(1)
+def notice(update:Update,context:CallbackContext):
+    if(isAllow(update.effective_chat.id)):
+        MyBot.send_message(
+            chat_id = update.effective_chat.id,
+            text = "So, what is the message that you want to sent to all users..."
+        )
+        return sendnoticetousers
+    else:
+        MyBot.send_message(
+                chat_id = update.effective_chat.id,
+                text = "This access are only given to Admin.So, Sorry!"
+            )
+        return ConversationHandler.END
+
+def sendnotice(update:Update,context:CallbackContext):
+    noticeis = update.message.text
+    for i in userIDlist():
+        if(i!=""):
+            MyBot.send_message(
+                    chat_id = int(i),
+                    text = noticeis
+                )
+    MyBot.send_message(
+        chat_id = update.effective_chat.id,
+        text = "noticed has been send to everyone.."
+    )
+    return ConversationHandler.END
